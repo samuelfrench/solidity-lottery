ffb26540b40dd1614bd88ff76a516a5aa689b32d
Sam French <samfrench@gmail.com>
2021-09-16 07:10:57 -0500

    add test asserting rollback for insufficient entry fee

--- test/testLotto.js
+++ test/testLotto.js
@@ -1,3 +1,4 @@
+const truffleAssert = require('truffle-assertions');
 const Lotto = artifacts.require("Lotto");
 
 //TODO: Format file
@@ -7,27 +8,33 @@ contract("Lotto", async accounts => {
     let lotto;
 
     beforeEach(async () => {
-        lotto = await Lotto.deployed();
+        lotto = await Lotto.new();
+        let balanceBefore = await lotto.getLotteryBalance.call();
+        assert.equal(balanceBefore, 0);
+        let entrantCountBefore = await lotto.getQuantityOfEntrants.call();
+        assert.equal(entrantCountBefore, 0);
     });
 
-  it("expecting 0 balance at contract creation", async () => {
-    let balance = await lotto.getLotteryBalance.call()
-    assert.equal(balance, 0);
-  });
-
   it("allows lottery entry", async () => {
-    let balanceBefore = await lotto.getLotteryBalance.call()
-    assert.equal(balanceBefore, 0);
-    let entrantCountBefore = await lotto.getQuantityOfEntrants.call()
-    assert.equal(entrantCountBefore, 0);
+    let enterResult = await lotto.enter({value: 500000000000000}); //TODO is this variable used
 
-    let enterResult = await lotto.enter({value: 500000000000000});
-
-    let balanceAfter = await lotto.getLotteryBalance.call()
+    let balanceAfter = await lotto.getLotteryBalance.call();
     assert.equal(balanceAfter, 500000000000000);
-    let entrantCountAfter = await lotto.getQuantityOfEntrants.call()
+    let entrantCountAfter = await lotto.getQuantityOfEntrants.call();
     assert.equal(entrantCountAfter, 1);
   });
+
+
+
+  it("prevents lottery entry if inadequate value provided in message", async() => {
+
+    truffleAssert.reverts(lotto.enter({value: 400000000000000}), "Invalid entry fee provided.");
+
+    let balanceAfter = await lotto.getLotteryBalance.call();
+    assert.equal(balanceAfter, 0);
+    let entrantCountAfter = await lotto.getQuantityOfEntrants.call();
+    assert.equal(entrantCountAfter, 0);
+  })
 
   //TODO: re-write tests from Lotto_test.sol
 });
