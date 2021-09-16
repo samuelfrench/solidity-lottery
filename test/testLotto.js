1290bb368344bae408b4357976cfb81bd08b8948
Sam French <samfrench@gmail.com>
2021-09-16 15:52:37 -0500

    working test for winner already selected entry case (woohoogit add test/testLotto.js )

--- test/testLotto.js
+++ test/testLotto.js
@@ -9,6 +9,7 @@ contract("Lotto", async accounts => {
 
     beforeEach(async () => {
         lotto = await Lotto.new();
+
         let balanceBefore = await lotto.getLotteryBalance.call();
         assert.equal(balanceBefore, 0);
         let entrantCountBefore = await lotto.getQuantityOfEntrants.call();
@@ -50,6 +51,44 @@ contract("Lotto", async accounts => {
         await truffleAssert.reverts(lotto.enter({value: 500000000000000}), "User has already entered. Only one entry allowed per address.");
     });
 
+    it("prevents entry into the lottery if winner selection is in progress", async() => {
+        let enterResult = await lotto.enter({value: 500000000000000});
+        await lotto.selectWinner();
+
+        await truffleAssert.reverts(lotto.enter({value: 500000000000000, from: accounts[1]}),
+            "Winner selection already in progress. No entries allowed now.");
+    });
+
+    //TODO clean this up
+    it("prevents entry into the lottery once a winner has already been selected", async() => {
+        let enterResult = await lotto.enter({value: 500000000000000});
+
+        let result = await lotto.selectWinner();
+
+        await truffleAssert.eventEmitted(result, 'LogWinnerSelectionStarted');
+
+        let events = await lotto.getPastEvents( 'LogWinnerSelected', { fromBlock: 0, toBlock: 'latest' } )
+
+        let secondCounter = 0;
+        const sleep = ms => new Promise(res => setTimeout(res, ms));
+        while(events.length < 1){
+            console.log("polling for winner selected event");
+            await sleep(1000);
+            secondCounter++;
+            events = await lotto.getPastEvents( 'LogWinnerSelected', { fromBlock: 0, toBlock: 'latest' } )
+            if(secondCounter > 100){
+                assert(false);
+            }
+        }
+
+        await truffleAssert.reverts(lotto.enter({value: 500000000000000, from: accounts[1]}),
+                    "Lottery has already completed. A winner was already selected.");
+    });
 
   //TODO: re-write tests from Lotto_test.sol
+  const getFirstEvent = (_event) => {
+    return new Promise((resolve, reject) => {
+      _event.once('data', resolve).once('error', reject)
+    });
+  }
 });
