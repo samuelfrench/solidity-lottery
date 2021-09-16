6a7b0a20938e87263b00bcb2525536e6ede7dd0e
Sam French <samfrench@gmail.com>
2021-09-16 09:37:55 -0500

    add additional test cases for entry function

--- test/testLotto.js
+++ test/testLotto.js
@@ -26,15 +26,30 @@ contract("Lotto", async accounts => {
 
 
 
-  it("prevents lottery entry if inadequate value provided in message", async() => {
-
-    truffleAssert.reverts(lotto.enter({value: 400000000000000}), "Invalid entry fee provided.");
+  it("prevents lottery entry if insufficient entry fee provided", async() => {
+    await truffleAssert.reverts(lotto.enter({value: 400000000000000}), "Invalid entry fee provided.");
 
     let balanceAfter = await lotto.getLotteryBalance.call();
     assert.equal(balanceAfter, 0);
     let entrantCountAfter = await lotto.getQuantityOfEntrants.call();
     assert.equal(entrantCountAfter, 0);
-  })
+  });
+
+    it("prevents lottery entry if entry fee provided is greater than what's required", async() => {
+      await truffleAssert.reverts(lotto.enter({value: 600000000000000}), "Invalid entry fee provided.");
+
+      let balanceAfter = await lotto.getLotteryBalance.call();
+      assert.equal(balanceAfter, 0);
+      let entrantCountAfter = await lotto.getQuantityOfEntrants.call();
+      assert.equal(entrantCountAfter, 0);
+    });
+
+    it("prevents lottery entry if the address has already been entered into the lottery", async() => {
+        let enterResult = await lotto.enter({value: 500000000000000});
+
+        await truffleAssert.reverts(lotto.enter({value: 500000000000000}), "User has already entered. Only one entry allowed per address.");
+    });
+
 
   //TODO: re-write tests from Lotto_test.sol
 });
