e69d5f936f600d35ef0ffddfb6b352f7486d6d51
Sam French <samfrench@gmail.com>
2021-09-16 06:46:19 -0500

    move common test setup to beforeEach

--- test/testLotto.js
+++ test/testLotto.js
@@ -1,24 +1,31 @@
 const Lotto = artifacts.require("Lotto");
 
+//TODO: Format file
+
 contract("Lotto", async accounts => {
+
+    let lotto;
+
+    beforeEach(async () => {
+        lotto = await Lotto.deployed();
+    });
+
   it("expecting 0 balance at contract creation", async () => {
-    let instance = await Lotto.deployed(); //TODO move to beforeEach
-    let balance = await instance.getLotteryBalance.call()
+    let balance = await lotto.getLotteryBalance.call()
     assert.equal(balance, 0);
   });
 
   it("allows lottery entry", async () => {
-    let instance = await Lotto.deployed(); //TODO move to beforeEach
-    let balanceBefore = await instance.getLotteryBalance.call()
+    let balanceBefore = await lotto.getLotteryBalance.call()
     assert.equal(balanceBefore, 0);
-    let entrantCountBefore = await instance.getQuantityOfEntrants.call()
+    let entrantCountBefore = await lotto.getQuantityOfEntrants.call()
     assert.equal(entrantCountBefore, 0);
 
-    let enterResult = await instance.enter({value: 500000000000000});
+    let enterResult = await lotto.enter({value: 500000000000000});
 
-    let balanceAfter = await instance.getLotteryBalance.call()
+    let balanceAfter = await lotto.getLotteryBalance.call()
     assert.equal(balanceAfter, 500000000000000);
-    let entrantCountAfter = await instance.getQuantityOfEntrants.call()
+    let entrantCountAfter = await lotto.getQuantityOfEntrants.call()
     assert.equal(entrantCountAfter, 1);
   });
 
