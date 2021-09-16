29231d2978af52679775e010cec38213e781be47
Sam French <samfrench@gmail.com>
2021-09-15 22:03:23 -0500

    add happy path test

--- test/testLotto.js
+++ test/testLotto.js
@@ -2,8 +2,23 @@ const Lotto = artifacts.require("Lotto");
 
 contract("Lotto", async accounts => {
   it("expecting 0 balance at contract creation", async () => {
-    let instance = await Lotto.deployed();
+    let instance = await Lotto.deployed(); //TODO move to beforeEach
     let balance = await instance.getLotteryBalance.call()
     assert.equal(balance, 0);
+  });
+
+  it("allows lottery entry", async () => {
+    let instance = await Lotto.deployed(); //TODO move to beforeEach
+    let balanceBefore = await instance.getLotteryBalance.call()
+    assert.equal(balanceBefore, 0);
+    let entrantCountBefore = await instance.getQuantityOfEntrants.call()
+    assert.equal(entrantCountBefore, 0);
+
+    let enterResult = await instance.enter({value: 500000000000000});
+
+    let balanceAfter = await instance.getLotteryBalance.call()
+    assert.equal(balanceAfter, 500000000000000);
+    let entrantCountAfter = await instance.getQuantityOfEntrants.call()
+    assert.equal(entrantCountAfter, 1);
   });
 });
