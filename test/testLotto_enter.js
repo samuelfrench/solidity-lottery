3c1e4bd4751253e50ea1a656cbff8dfc8ca71419
Sam French <samfrench@gmail.com>
2021-09-20 14:08:28 -0500

    misc test cleanup

--- test/testLotto_enter.js
+++ test/testLotto_enter.js
@@ -1,5 +1,5 @@
 const truffleAssert = require('truffle-assertions');
-const { waitForEvent, validEnterValue } = require('./utils')
+const { waitForEvent, validEntryValue } = require('./utils')
 
 const Lotto = artifacts.require('Lotto');
 
@@ -16,26 +16,26 @@ contract('Lotto', async (accounts) => {
   });
 
   it('allows lottery entry', async () => {
-    await lotto.enter({ value: validEnterValue});
+    await lotto.enter({ value: validEntryValue});
 
     const balanceAfter = await lotto.getLotteryBalance.call();
-    assert.equal(balanceAfter, validEnterValue);
+    assert.equal(balanceAfter, validEntryValue);
     const entrantCountAfter = await lotto.getQuantityOfEntrants.call();
     assert.equal(entrantCountAfter, 1);
   });
 
   it('allows lottery entry with multiple entrants', async () => {
-    await lotto.enter({ value: validEnterValue });
-    await lotto.enter({ value: validEnterValue, from: accounts[1] });
+    await lotto.enter({ value: validEntryValue });
+    await lotto.enter({ value: validEntryValue, from: accounts[1] });
 
     const balanceAfter = await lotto.getLotteryBalance.call();
-    assert.equal(balanceAfter, validEnterValue*2);
+    assert.equal(balanceAfter, validEntryValue*2);
     const entrantCountAfter = await lotto.getQuantityOfEntrants.call();
     assert.equal(entrantCountAfter, 2);
   });
 
   it('prevents lottery entry if insufficient entry fee provided', async () => {
-    await truffleAssert.reverts(lotto.enter({ value: validEnterValue-1 }), 'Invalid entry fee provided.');
+    await truffleAssert.reverts(lotto.enter({ value: validEntryValue-1 }), 'Invalid entry fee provided.');
 
     const balanceAfter = await lotto.getLotteryBalance.call();
     assert.equal(balanceAfter, 0);
@@ -44,7 +44,7 @@ contract('Lotto', async (accounts) => {
   });
 
   it("prevents lottery entry if entry fee provided is greater than what's required", async () => {
-    await truffleAssert.reverts(lotto.enter({ value: validEnterValue+1 }), 'Invalid entry fee provided.');
+    await truffleAssert.reverts(lotto.enter({ value: validEntryValue+1 }), 'Invalid entry fee provided.');
 
     const balanceAfter = await lotto.getLotteryBalance.call();
     assert.equal(balanceAfter, 0);
@@ -53,26 +53,26 @@ contract('Lotto', async (accounts) => {
   });
 
   it('prevents lottery entry if the address has already been entered into the lottery', async () => {
-    const enterResult = await lotto.enter({  value: validEnterValue });
+    const enterResult = await lotto.enter({  value: validEntryValue });
 
-    await truffleAssert.reverts(lotto.enter({  value: validEnterValue }), 'User has already entered. Only one entry allowed per address.');
+    await truffleAssert.reverts(lotto.enter({  value: validEntryValue }), 'User has already entered. Only one entry allowed per address.');
   });
 
   it('prevents entry into the lottery if winner selection is in progress', async () => {
-    const enterResult = await lotto.enter({  value: validEnterValue });
+    const enterResult = await lotto.enter({  value: validEntryValue });
     await lotto.selectWinner();
 
-    await truffleAssert.reverts(lotto.enter({  value: validEnterValue, from: accounts[1] }),
+    await truffleAssert.reverts(lotto.enter({  value: validEntryValue, from: accounts[1] }),
       'Winner selection already in progress. No entries allowed now.');
   });
 
   it('prevents entry into the lottery once a winner has already been selected', async () => {
-    await lotto.enter({ value: validEnterValue });
+    await lotto.enter({ value: validEntryValue });
     const selectWinnerResult = await lotto.selectWinner();
     await truffleAssert.eventEmitted(selectWinnerResult, 'LogWinnerSelectionStarted');
     await waitForEvent('LogWinnerSelected', lotto);
 
-    await truffleAssert.reverts(lotto.enter({ value: validEnterValue, from: accounts[1] }),
+    await truffleAssert.reverts(lotto.enter({ value: validEntryValue, from: accounts[1] }),
       'Lottery has already completed. A winner was already selected.');
   });
 });
--- test/utils.js
+++ test/utils.js
@@ -5,14 +5,14 @@ module.exports.waitForEvent = async (eventName, contract) => {
                                 let secondCounter = 0;
                                 const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
                                 while (events.length < 1) {
-                                  console.log('waiting for event');
+                                  console.log('waiting for event ' + secondCounter);
                                   await sleep(1000);
                                   secondCounter++;
                                   events = await contract.getPastEvents(eventName, { fromBlock: 0, toBlock: 'latest' });
-                                  if (secondCounter > 100) {
-                                    assert(false);
+                                  if (secondCounter > 30) {
+                                    assert(false, "Timed out waiting for event: " + eventName);
                                   }
                                 }
                               };
 
-module.exports.validEnterValue = Web3.utils.toWei('500000', 'gwei');
+module.exports.validEntryValue = Web3.utils.toWei('500000', 'gwei');
