64d500f5f7231d5379aeffdafa736b4ea0d95fb0
Sam French <samfrench@gmail.com>
2021-09-27 15:43:10 -0500

    demonstrate multiple approaches to testing in solidity unit testing

--- legacy_test/Lotto_test.sol
+++ legacy_test/Lotto_test.sol
@@ -14,17 +14,14 @@ import "remix_accounts.sol";
 import "./LottoMock.sol";
 import "../contracts/Lotto.sol";
 
-//TODO all this needs more testing
-//TODO is lottomock needed? - verify at end
+//this style of inheriting from the class under test allows for impersonating senders but causes issues when checking the contract balance (because the test context adds value)
 contract lottoEntranceTest is LottoMock {
-    //ENTRY test cases
 
     ///case 1: when: fee too much -> then: return money, don't enter
-    /// #sender: account-0
     /// #value: 6000000000000000
     function enterEntryFeeExceedsRequirement() public payable {
         Assert.equal(getQuantityOfEntrants(), uint256(0), "expecting 0 entrants before entering");
-        Assert.equal(getLotteryBalance(), uint256(6000000000000000), "expecting 0 lottery balance before entering"); //TODO this seems like an oddity with how the custom txn context is implemented with inheritance (could make a simplified case and report another issue)
+        Assert.equal(getLotteryBalance(), uint256(6000000000000000), "expecting 0 lottery balance before entering"); //this seems like an oddity with how the custom txn context is implemented with inheritance
 
         try this.enter{value:6000000000000000}() {
             Assert.ok(false, 'succeed unexpected');
@@ -34,21 +31,50 @@ contract lottoEntranceTest is LottoMock {
             Assert.ok(false, 'failed unexpected');
         }
 
-        Assert.equal(getLotteryBalance(), uint256(6000000000000000), "expecting lottery balance equal to entrance fee after entering"); //TODO this seems like an oddity with how the custom txn context is implemented with inheritance
+        Assert.equal(getLotteryBalance(), uint256(6000000000000000), "expecting lottery balance equal to entrance fee after entering"); //this seems like an oddity with how the custom txn context is implemented with inheritance
         Assert.equal(this.getQuantityOfEntrants(), uint256(0), "user should not have successfully entered the lottery");
     }
 
     ///case 6: enter successfully
-    /// #sender: account-1
     /// #value: 5000000000000000
     function enterSuccessfullySingleEntrant() public payable {
         Assert.equal(getQuantityOfEntrants(), uint256(0), "expecting 0 entrants before entering");
-        Assert.equal(getLotteryBalance(), uint256(11000000000000000), "expecting 0 lottery balance before entering"); //TODO this seems like an oddity with how the custom txn context is implemented with inheritance
+        Assert.equal(getLotteryBalance(), uint256(11000000000000000), "expecting 0 lottery balance before entering"); //this seems like an oddity with how the custom txn context is implemented with inheritance
 
         this.enter{value:5000000000000000}();
 
-        Assert.equal(getLotteryBalance(), uint256(11000000000000000), "expecting lottery balance equal to entrance fee after entering");  //TODO this seems like an oddity with how the custom txn context is implemented with inheritance
+        Assert.equal(getLotteryBalance(), uint256(11000000000000000), "expecting lottery balance equal to entrance fee after entering");  //this seems like an oddity with how the custom txn context is implemented with inheritance
         Assert.equal(getQuantityOfEntrants(), uint256(1), "user should have successfully entered the lottery");
+    }
+}
+
+//this style works for checking contract balance but doesn't let you proprerly impersonate multiple senders
+contract LottoEntranceTestNoInherit {
+    Lotto l;
+
+    function beforeEach() public {
+        l = new Lotto();
+    }
+    ///case 3: when already entered -> then: return money, don't enter
+    /// #value: 10000000000000000
+    function enterAlreadyEntered() public payable {
+        Assert.equal(l.getQuantityOfEntrants(), uint256(0), "expecting 0 entrants before entering");
+        Assert.equal(l.getLotteryBalance(), uint256(0), "expecting 0 lottery balance before entering");
+        l.enter{value:5000000000000000}();
+        Assert.equal(l.getLotteryBalance(), uint256(5000000000000000), "expecting lottery balance equal to entrance fee after entering");
+        Assert.equal(l.getQuantityOfEntrants(), uint256(1), "user should have successfully entered the lottery");
+
+
+        try l.enter{value:5000000000000000}() {
+            Assert.ok(false, 'succeed unexpected');
+        } catch Error(string memory reason) {
+            Assert.equal(reason, "User has already entered. Only one entry allowed per address.", "Expected failure, user has already entered.");
+        } catch (bytes memory ) {
+            Assert.ok(false, 'failed unexpected');
+        }
+
+        Assert.equal(l.getLotteryBalance(), uint256(5000000000000000), "Lottery balance should be unchanged after failed entry");
+        Assert.equal(l.getQuantityOfEntrants(), uint256(1), "User has already entered, only expecting 1 entrant.");
     }
 }
 
@@ -98,29 +124,6 @@ contract lottoMultipleEntranceTest is Lotto {
 
         Assert.equal(this.getLotteryBalance(), uint256(0), "expecting lottery balance equal to entrance fee after entering");
         Assert.equal(this.getQuantityOfEntrants(), uint256(0), "user should have successfully entered the lottery");
-    }
-
-    ///case 3: when already entered -> then: return money, don't enter
-    /// #sender: account-0
-    /// #value: 1000000000000000
-    function enterAlreadyEntered() public payable {
-        Assert.equal(this.getQuantityOfEntrants(), uint256(0), "expecting 0 entrants before entering");
-        Assert.equal(this.getLotteryBalance(), uint256(0), "expecting 0 lottery balance before entering");
-        this.enter{value:5000000000000000}();
-        Assert.equal(this.getLotteryBalance(), uint256(5000000000000000), "expecting lottery balance equal to entrance fee after entering");
-        Assert.equal(this.getQuantityOfEntrants(), uint256(1), "user should have successfully entered the lottery");
-
-
-        try this.enter{value:5000000000000000}() {
-            Assert.ok(false, 'succeed unexpected');
-        } catch Error(string memory reason) {
-            Assert.equal(reason, "User has already entered. Only one entry allowed per address.", "Expected failure, user has already entered.");
-        } catch (bytes memory ) {
-            Assert.ok(false, 'failed unexpected');
-        }
-
-        Assert.equal(this.getLotteryBalance(), uint256(5000000000000000), "Lottery balance should be unchanged after failed entry");
-        Assert.equal(this.getQuantityOfEntrants(), uint256(1), "User has already entered, only expecting 1 entrant.");
     }
 
     ///case 4: lottery already completed -> then: return money, don't enter
